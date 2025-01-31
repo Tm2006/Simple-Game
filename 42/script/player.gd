e33a130899b453f093b3ea9e8fac4a3f7047d23f
Tm2006 <timon.mihalev@gmail.com>
2025-02-01 02:11:18 +0300

    test
    yo

---
+++ 42/script/player.gd
@@ -0,0 +1,136 @@
+extends CharacterBody2D
+
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var heatlh = 160
+var player_alive = true
+
+var attack_ip = false
+
+const speed = 100
+var current_dir = "none"
+
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	player_movement(delta)
+	enemy_attack()
+	attack()
+	
+	if heatlh <= 0:
+		player_alive = false  #go back to respawn
+		heatlh = 0
+		print("player has been killed")
+		self.queue_free()
+
+func player_movement(delta):
+	if Input.get_action_strength("ui_right"):
+		play_anim(1)
+		current_dir = "right"
+	elif Input.get_action_strength("ui_left"):
+		play_anim(1)
+		current_dir = "left"
+	elif Input.get_action_strength("ui_down"):
+		play_anim(1)
+		current_dir = "down"
+	elif Input.get_action_strength("ui_up"):
+		play_anim(1)
+		current_dir = "up"
+	else:
+		play_anim(0)
+	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector:
+		velocity = input_vector * speed
+	else:
+		velocity = input_vector
+	
+	move_and_slide()
+	
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			if attack_ip == false:
+				anim.play("side_idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			if attack_ip == false:
+				anim.play("side_idle")
+	if dir == "down":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("front_walk")
+		elif movement == 0:
+			if attack_ip == false:
+				anim.play("front_idle")
+	if dir == "up":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("back_walk")
+		elif movement == 0:
+			if attack_ip == false:
+				anim.play("back_idle")
+
+
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		heatlh = heatlh - 20
+		enemy_attack_cooldown = false
+		$atack_cooldown.start()
+		print(heatlh)
+
+func _on_atack_cooldown_timeout():
+	enemy_attack_cooldown = true
+
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		attack_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+			$deal_attack_timer.start()
+		if dir == "up":
+			$AnimatedSprite2D.play("back_attack")
+			$deal_attack_timer.start()
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	global.player_current_attack = false
+	attack_ip = false
